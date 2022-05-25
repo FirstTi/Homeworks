8439a3242b491bb36f62fc849db96ce725b6bfd7
FirstTi <JustInSluchay@protonmail.com>
2022-05-25 23:17:42 +0300

    1

---
+++ Lesson4/Easy/Country.py
@@ -0,0 +1,3 @@
+# написать программу с пустым классом Counrty. Описать наследуемые от класса Counrty классы Bulgaria,
+# Germany, Canada. Дабавить каждому классу поле population и описать метод setPopulation(задать 
+# обьекту свойство population) и getPopulation(получить из обьекта сво-во population).
